4a6529eb212ce887389e1f93c1408405b199e5bb
opaL1022 <whitestoneabc123@gmail.com>
2025-06-28 23:22:15 +0800

    fix link not working

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
       <main className="content">
         {/* 例外：把這行標題整個置中，字級放大 */}
         <div className="text-center mb-4">
-          <Link href="https://drive.google.com/drive/folders/…" className="text-2xl hover:text-[#98AFC7]">
+          <Link href="https://drive.google.com/drive/folders/1zoRayCwbnTJIZ_WJSXRyR0Z5-BThec8X" className="text-2xl hover:text-[#98AFC7]">
             考古題
           </Link>
         </div>
